f4a6416066e2d06837540e83dbab5b233d1098ec
Abraomukas <abraomukas@gmail.com>
2023-01-11 17:21:50 +0400

    feat: add new endpoint to create new users

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// MongoDB
+
+mongoose.connect(process.env.MONGO_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const userSchema = new mongoose.Schema({
+	username: String,
+});
+
+let ExerciseUser = mongoose.model('ExerciseUser', userSchema);
+
+const exerciseSchema = new mongoose.Schema({
+	description: String,
+	duration: Number,
+	date: String,
+});
+
+let Exercise = mongoose.model('Exercise', exerciseSchema);
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/views/index.html');
 });
@@ -13,11 +40,28 @@ app.get('/', (req, res) => {
  * POST
  * Create a new user
  */
-app.post('/api/users', function (req, res) {});
+app.post('/api/users', function (req, res) {
+	const inputUsername = req.body.username;
+
+	console.log(
+		'creating a new user with username - '.toLocaleUpperCase() + inputUsername
+	);
+	let newUser = new ExerciseUser({ username: inputUsername });
+
+	newUser.save((err, user) => {
+		if (err) {
+			console.error(err);
+			res.json({ message: 'User creation failed! Something went wrong...' });
+		}
+
+		res.json({ message: 'User creation successful!', user: user });
+	});
+});
 
 /**
  * POST
  * Add a new exercise
+ * @param _id
  */
 app.post('/api/users/:_id/exercises', function (req, res) {});
 
@@ -25,7 +69,7 @@ app.post('/api/users/:_id/exercises', function (req, res) {});
  * GET
  * Get a user's exercise log
  */
-app.get('', function (req, res) {});
+app.get('/api/users/:_id/logs?from&to&limit', function (req, res) {});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
 	console.log('Your app is listening on port ' + listener.address().port);
